f77949827a435915f33b73b784f482fbdfa3cea3
selirasid <selghiulrasid@Selghiuls-MacBook-Pro.local>
2025-01-25 14:41:38 +0200

    Added functions to get song details

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+ Homework 1: Variables
+ My favourite song
+"""
+
+Song = "Smells like teen spirit" #Song name - String
+Artist = "Nirvana" #Artist - String
+Genre = "Grunge" #String
+Released_year = 1991 #Int
+Album = "Nevermind" #Stringss
+
+if __name__ == "__main__":
+    print(Song)
+    print(Artist)
+    print(Genre)
+    print(Released_year)
+    print(Album)
+
